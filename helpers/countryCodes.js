23bed80f218432cc06925402341e9b74eb6ab8f4
Fershad Irani <27988517+fershad@users.noreply.github.com>
2023-02-12 16:40:03 +0800

    add 2- and 3-digit country codes

---
+++ helpers/countryCodes.js
@@ -0,0 +1,1000 @@
+// Taken from https://gist.github.com/ssskip/5a94bfcd2835bf1dea52?permalink_comment_id=3871534#gistcomment-3871534
+
+export const codes = [
+  {
+    alpha2Code: "AF",
+    alpha3Code: "AFG",
+  },
+  {
+    alpha2Code: "AL",
+    alpha3Code: "ALB",
+  },
+  {
+    alpha2Code: "DZ",
+    alpha3Code: "DZA",
+  },
+  {
+    alpha2Code: "AS",
+    alpha3Code: "ASM",
+  },
+  {
+    alpha2Code: "AD",
+    alpha3Code: "AND",
+  },
+  {
+    alpha2Code: "AO",
+    alpha3Code: "AGO",
+  },
+  {
+    alpha2Code: "AI",
+    alpha3Code: "AIA",
+  },
+  {
+    alpha2Code: "AQ",
+    alpha3Code: "ATA",
+  },
+  {
+    alpha2Code: "AG",
+    alpha3Code: "ATG",
+  },
+  {
+    alpha2Code: "AR",
+    alpha3Code: "ARG",
+  },
+  {
+    alpha2Code: "AM",
+    alpha3Code: "ARM",
+  },
+  {
+    alpha2Code: "AW",
+    alpha3Code: "ABW",
+  },
+  {
+    alpha2Code: "AU",
+    alpha3Code: "AUS",
+  },
+  {
+    alpha2Code: "AT",
+    alpha3Code: "AUT",
+  },
+  {
+    alpha2Code: "AZ",
+    alpha3Code: "AZE",
+  },
+  {
+    alpha2Code: "BS",
+    alpha3Code: "BHS",
+  },
+  {
+    alpha2Code: "BH",
+    alpha3Code: "BHR",
+  },
+  {
+    alpha2Code: "BD",
+    alpha3Code: "BGD",
+  },
+  {
+    alpha2Code: "BB",
+    alpha3Code: "BRB",
+  },
+  {
+    alpha2Code: "BY",
+    alpha3Code: "BLR",
+  },
+  {
+    alpha2Code: "BE",
+    alpha3Code: "BEL",
+  },
+  {
+    alpha2Code: "BZ",
+    alpha3Code: "BLZ",
+  },
+  {
+    alpha2Code: "BJ",
+    alpha3Code: "BEN",
+  },
+  {
+    alpha2Code: "BM",
+    alpha3Code: "BMU",
+  },
+  {
+    alpha2Code: "BT",
+    alpha3Code: "BTN",
+  },
+  {
+    alpha2Code: "BO",
+    alpha3Code: "BOL",
+  },
+  {
+    alpha2Code: "BQ",
+    alpha3Code: "BES",
+  },
+  {
+    alpha2Code: "BA",
+    alpha3Code: "BIH",
+  },
+  {
+    alpha2Code: "BW",
+    alpha3Code: "BWA",
+  },
+  {
+    alpha2Code: "BV",
+    alpha3Code: "BVT",
+  },
+  {
+    alpha2Code: "BR",
+    alpha3Code: "BRA",
+  },
+  {
+    alpha2Code: "IO",
+    alpha3Code: "IOT",
+  },
+  {
+    alpha2Code: "BN",
+    alpha3Code: "BRN",
+  },
+  {
+    alpha2Code: "BG",
+    alpha3Code: "BGR",
+  },
+  {
+    alpha2Code: "BF",
+    alpha3Code: "BFA",
+  },
+  {
+    alpha2Code: "BI",
+    alpha3Code: "BDI",
+  },
+  {
+    alpha2Code: "CV",
+    alpha3Code: "CPV",
+  },
+  {
+    alpha2Code: "KH",
+    alpha3Code: "KHM",
+  },
+  {
+    alpha2Code: "CM",
+    alpha3Code: "CMR",
+  },
+  {
+    alpha2Code: "CA",
+    alpha3Code: "CAN",
+  },
+  {
+    alpha2Code: "KY",
+    alpha3Code: "CYM",
+  },
+  {
+    alpha2Code: "CF",
+    alpha3Code: "CAF",
+  },
+  {
+    alpha2Code: "TD",
+    alpha3Code: "TCD",
+  },
+  {
+    alpha2Code: "CL",
+    alpha3Code: "CHL",
+  },
+  {
+    alpha2Code: "CN",
+    alpha3Code: "CHN",
+  },
+  {
+    alpha2Code: "CX",
+    alpha3Code: "CXR",
+  },
+  {
+    alpha2Code: "CC",
+    alpha3Code: "CCK",
+  },
+  {
+    alpha2Code: "CO",
+    alpha3Code: "COL",
+  },
+  {
+    alpha2Code: "KM",
+    alpha3Code: "COM",
+  },
+  {
+    alpha2Code: "CD",
+    alpha3Code: "COD",
+  },
+  {
+    alpha2Code: "CG",
+    alpha3Code: "COG",
+  },
+  {
+    alpha2Code: "CK",
+    alpha3Code: "COK",
+  },
+  {
+    alpha2Code: "CR",
+    alpha3Code: "CRI",
+  },
+  {
+    alpha2Code: "HR",
+    alpha3Code: "HRV",
+  },
+  {
+    alpha2Code: "CU",
+    alpha3Code: "CUB",
+  },
+  {
+    alpha2Code: "CW",
+    alpha3Code: "CUW",
+  },
+  {
+    alpha2Code: "CY",
+    alpha3Code: "CYP",
+  },
+  {
+    alpha2Code: "CZ",
+    alpha3Code: "CZE",
+  },
+  {
+    alpha2Code: "CI",
+    alpha3Code: "CIV",
+  },
+  {
+    alpha2Code: "DK",
+    alpha3Code: "DNK",
+  },
+  {
+    alpha2Code: "DJ",
+    alpha3Code: "DJI",
+  },
+  {
+    alpha2Code: "DM",
+    alpha3Code: "DMA",
+  },
+  {
+    alpha2Code: "DO",
+    alpha3Code: "DOM",
+  },
+  {
+    alpha2Code: "EC",
+    alpha3Code: "ECU",
+  },
+  {
+    alpha2Code: "EG",
+    alpha3Code: "EGY",
+  },
+  {
+    alpha2Code: "SV",
+    alpha3Code: "SLV",
+  },
+  {
+    alpha2Code: "GQ",
+    alpha3Code: "GNQ",
+  },
+  {
+    alpha2Code: "ER",
+    alpha3Code: "ERI",
+  },
+  {
+    alpha2Code: "EE",
+    alpha3Code: "EST",
+  },
+  {
+    alpha2Code: "SZ",
+    alpha3Code: "SWZ",
+  },
+  {
+    alpha2Code: "ET",
+    alpha3Code: "ETH",
+  },
+  {
+    alpha2Code: "FK",
+    alpha3Code: "FLK",
+  },
+  {
+    alpha2Code: "FO",
+    alpha3Code: "FRO",
+  },
+  {
+    alpha2Code: "FJ",
+    alpha3Code: "FJI",
+  },
+  {
+    alpha2Code: "FI",
+    alpha3Code: "FIN",
+  },
+  {
+    alpha2Code: "FR",
+    alpha3Code: "FRA",
+  },
+  {
+    alpha2Code: "GF",
+    alpha3Code: "GUF",
+  },
+  {
+    alpha2Code: "PF",
+    alpha3Code: "PYF",
+  },
+  {
+    alpha2Code: "TF",
+    alpha3Code: "ATF",
+  },
+  {
+    alpha2Code: "GA",
+    alpha3Code: "GAB",
+  },
+  {
+    alpha2Code: "GM",
+    alpha3Code: "GMB",
+  },
+  {
+    alpha2Code: "GE",
+    alpha3Code: "GEO",
+  },
+  {
+    alpha2Code: "DE",
+    alpha3Code: "DEU",
+  },
+  {
+    alpha2Code: "GH",
+    alpha3Code: "GHA",
+  },
+  {
+    alpha2Code: "GI",
+    alpha3Code: "GIB",
+  },
+  {
+    alpha2Code: "GR",
+    alpha3Code: "GRC",
+  },
+  {
+    alpha2Code: "GL",
+    alpha3Code: "GRL",
+  },
+  {
+    alpha2Code: "GD",
+    alpha3Code: "GRD",
+  },
+  {
+    alpha2Code: "GP",
+    alpha3Code: "GLP",
+  },
+  {
+    alpha2Code: "GU",
+    alpha3Code: "GUM",
+  },
+  {
+    alpha2Code: "GT",
+    alpha3Code: "GTM",
+  },
+  {
+    alpha2Code: "GG",
+    alpha3Code: "GGY",
+  },
+  {
+    alpha2Code: "GN",
+    alpha3Code: "GIN",
+  },
+  {
+    alpha2Code: "GW",
+    alpha3Code: "GNB",
+  },
+  {
+    alpha2Code: "GY",
+    alpha3Code: "GUY",
+  },
+  {
+    alpha2Code: "HT",
+    alpha3Code: "HTI",
+  },
+  {
+    alpha2Code: "HM",
+    alpha3Code: "HMD",
+  },
+  {
+    alpha2Code: "VA",
+    alpha3Code: "VAT",
+  },
+  {
+    alpha2Code: "HN",
+    alpha3Code: "HND",
+  },
+  {
+    alpha2Code: "HK",
+    alpha3Code: "HKG",
+  },
+  {
+    alpha2Code: "HU",
+    alpha3Code: "HUN",
+  },
+  {
+    alpha2Code: "IS",
+    alpha3Code: "ISL",
+  },
+  {
+    alpha2Code: "IN",
+    alpha3Code: "IND",
+  },
+  {
+    alpha2Code: "ID",
+    alpha3Code: "IDN",
+  },
+  {
+    alpha2Code: "IR",
+    alpha3Code: "IRN",
+  },
+  {
+    alpha2Code: "IQ",
+    alpha3Code: "IRQ",
+  },
+  {
+    alpha2Code: "IE",
+    alpha3Code: "IRL",
+  },
+  {
+    alpha2Code: "IM",
+    alpha3Code: "IMN",
+  },
+  {
+    alpha2Code: "IL",
+    alpha3Code: "ISR",
+  },
+  {
+    alpha2Code: "IT",
+    alpha3Code: "ITA",
+  },
+  {
+    alpha2Code: "JM",
+    alpha3Code: "JAM",
+  },
+  {
+    alpha2Code: "JP",
+    alpha3Code: "JPN",
+  },
+  {
+    alpha2Code: "JE",
+    alpha3Code: "JEY",
+  },
+  {
+    alpha2Code: "JO",
+    alpha3Code: "JOR",
+  },
+  {
+    alpha2Code: "KZ",
+    alpha3Code: "KAZ",
+  },
+  {
+    alpha2Code: "KE",
+    alpha3Code: "KEN",
+  },
+  {
+    alpha2Code: "KI",
+    alpha3Code: "KIR",
+  },
+  {
+    alpha2Code: "KP",
+    alpha3Code: "PRK",
+  },
+  {
+    alpha2Code: "KR",
+    alpha3Code: "KOR",
+  },
+  {
+    alpha2Code: "KW",
+    alpha3Code: "KWT",
+  },
+  {
+    alpha2Code: "KG",
+    alpha3Code: "KGZ",
+  },
+  {
+    alpha2Code: "LA",
+    alpha3Code: "LAO",
+  },
+  {
+    alpha2Code: "LV",
+    alpha3Code: "LVA",
+  },
+  {
+    alpha2Code: "LB",
+    alpha3Code: "LBN",
+  },
+  {
+    alpha2Code: "LS",
+    alpha3Code: "LSO",
+  },
+  {
+    alpha2Code: "LR",
+    alpha3Code: "LBR",
+  },
+  {
+    alpha2Code: "LY",
+    alpha3Code: "LBY",
+  },
+  {
+    alpha2Code: "LI",
+    alpha3Code: "LIE",
+  },
+  {
+    alpha2Code: "LT",
+    alpha3Code: "LTU",
+  },
+  {
+    alpha2Code: "LU",
+    alpha3Code: "LUX",
+  },
+  {
+    alpha2Code: "MO",
+    alpha3Code: "MAC",
+  },
+  {
+    alpha2Code: "MG",
+    alpha3Code: "MDG",
+  },
+  {
+    alpha2Code: "MW",
+    alpha3Code: "MWI",
+  },
+  {
+    alpha2Code: "MY",
+    alpha3Code: "MYS",
+  },
+  {
+    alpha2Code: "MV",
+    alpha3Code: "MDV",
+  },
+  {
+    alpha2Code: "ML",
+    alpha3Code: "MLI",
+  },
+  {
+    alpha2Code: "MT",
+    alpha3Code: "MLT",
+  },
+  {
+    alpha2Code: "MH",
+    alpha3Code: "MHL",
+  },
+  {
+    alpha2Code: "MQ",
+    alpha3Code: "MTQ",
+  },
+  {
+    alpha2Code: "MR",
+    alpha3Code: "MRT",
+  },
+  {
+    alpha2Code: "MU",
+    alpha3Code: "MUS",
+  },
+  {
+    alpha2Code: "YT",
+    alpha3Code: "MYT",
+  },
+  {
+    alpha2Code: "MX",
+    alpha3Code: "MEX",
+  },
+  {
+    alpha2Code: "FM",
+    alpha3Code: "FSM",
+  },
+  {
+    alpha2Code: "MD",
+    alpha3Code: "MDA",
+  },
+  {
+    alpha2Code: "MC",
+    alpha3Code: "MCO",
+  },
+  {
+    alpha2Code: "MN",
+    alpha3Code: "MNG",
+  },
+  {
+    alpha2Code: "ME",
+    alpha3Code: "MNE",
+  },
+  {
+    alpha2Code: "MS",
+    alpha3Code: "MSR",
+  },
+  {
+    alpha2Code: "MA",
+    alpha3Code: "MAR",
+  },
+  {
+    alpha2Code: "MZ",
+    alpha3Code: "MOZ",
+  },
+  {
+    alpha2Code: "MM",
+    alpha3Code: "MMR",
+  },
+  {
+    alpha2Code: "NA",
+    alpha3Code: "NAM",
+  },
+  {
+    alpha2Code: "NR",
+    alpha3Code: "NRU",
+  },
+  {
+    alpha2Code: "NP",
+    alpha3Code: "NPL",
+  },
+  {
+    alpha2Code: "NL",
+    alpha3Code: "NLD",
+  },
+  {
+    alpha2Code: "NC",
+    alpha3Code: "NCL",
+  },
+  {
+    alpha2Code: "NZ",
+    alpha3Code: "NZL",
+  },
+  {
+    alpha2Code: "NI",
+    alpha3Code: "NIC",
+  },
+  {
+    alpha2Code: "NE",
+    alpha3Code: "NER",
+  },
+  {
+    alpha2Code: "NG",
+    alpha3Code: "NGA",
+  },
+  {
+    alpha2Code: "NU",
+    alpha3Code: "NIU",
+  },
+  {
+    alpha2Code: "NF",
+    alpha3Code: "NFK",
+  },
+  {
+    alpha2Code: "MK",
+    alpha3Code: "MKD",
+  },
+  {
+    alpha2Code: "MP",
+    alpha3Code: "MNP",
+  },
+  {
+    alpha2Code: "NO",
+    alpha3Code: "NOR",
+  },
+  {
+    alpha2Code: "OM",
+    alpha3Code: "OMN",
+  },
+  {
+    alpha2Code: "PK",
+    alpha3Code: "PAK",
+  },
+  {
+    alpha2Code: "PW",
+    alpha3Code: "PLW",
+  },
+  {
+    alpha2Code: "PS",
+    alpha3Code: "PSE",
+  },
+  {
+    alpha2Code: "PA",
+    alpha3Code: "PAN",
+  },
+  {
+    alpha2Code: "PG",
+    alpha3Code: "PNG",
+  },
+  {
+    alpha2Code: "PY",
+    alpha3Code: "PRY",
+  },
+  {
+    alpha2Code: "PE",
+    alpha3Code: "PER",
+  },
+  {
+    alpha2Code: "PH",
+    alpha3Code: "PHL",
+  },
+  {
+    alpha2Code: "PN",
+    alpha3Code: "PCN",
+  },
+  {
+    alpha2Code: "PL",
+    alpha3Code: "POL",
+  },
+  {
+    alpha2Code: "PT",
+    alpha3Code: "PRT",
+  },
+  {
+    alpha2Code: "PR",
+    alpha3Code: "PRI",
+  },
+  {
+    alpha2Code: "QA",
+    alpha3Code: "QAT",
+  },
+  {
+    alpha2Code: "RO",
+    alpha3Code: "ROU",
+  },
+  {
+    alpha2Code: "RU",
+    alpha3Code: "RUS",
+  },
+  {
+    alpha2Code: "RW",
+    alpha3Code: "RWA",
+  },
+  {
+    alpha2Code: "RE",
+    alpha3Code: "REU",
+  },
+  {
+    alpha2Code: "BL",
+    alpha3Code: "BLM",
+  },
+  {
+    alpha2Code: "SH",
+    alpha3Code: "SHN",
+  },
+  {
+    alpha2Code: "KN",
+    alpha3Code: "KNA",
+  },
+  {
+    alpha2Code: "LC",
+    alpha3Code: "LCA",
+  },
+  {
+    alpha2Code: "MF",
+    alpha3Code: "MAF",
+  },
+  {
+    alpha2Code: "PM",
+    alpha3Code: "SPM",
+  },
+  {
+    alpha2Code: "VC",
+    alpha3Code: "VCT",
+  },
+  {
+    alpha2Code: "WS",
+    alpha3Code: "WSM",
+  },
+  {
+    alpha2Code: "SM",
+    alpha3Code: "SMR",
+  },
+  {
+    alpha2Code: "ST",
+    alpha3Code: "STP",
+  },
+  {
+    alpha2Code: "SA",
+    alpha3Code: "SAU",
+  },
+  {
+    alpha2Code: "SN",
+    alpha3Code: "SEN",
+  },
+  {
+    alpha2Code: "RS",
+    alpha3Code: "SRB",
+  },
+  {
+    alpha2Code: "SC",
+    alpha3Code: "SYC",
+  },
+  {
+    alpha2Code: "SL",
+    alpha3Code: "SLE",
+  },
+  {
+    alpha2Code: "SG",
+    alpha3Code: "SGP",
+  },
+  {
+    alpha2Code: "SX",
+    alpha3Code: "SXM",
+  },
+  {
+    alpha2Code: "SK",
+    alpha3Code: "SVK",
+  },
+  {
+    alpha2Code: "SI",
+    alpha3Code: "SVN",
+  },
+  {
+    alpha2Code: "SB",
+    alpha3Code: "SLB",
+  },
+  {
+    alpha2Code: "SO",
+    alpha3Code: "SOM",
+  },
+  {
+    alpha2Code: "ZA",
+    alpha3Code: "ZAF",
+  },
+  {
+    alpha2Code: "GS",
+    alpha3Code: "SGS",
+  },
+  {
+    alpha2Code: "SS",
+    alpha3Code: "SSD",
+  },
+  {
+    alpha2Code: "ES",
+    alpha3Code: "ESP",
+  },
+  {
+    alpha2Code: "LK",
+    alpha3Code: "LKA",
+  },
+  {
+    alpha2Code: "SD",
+    alpha3Code: "SDN",
+  },
+  {
+    alpha2Code: "SR",
+    alpha3Code: "SUR",
+  },
+  {
+    alpha2Code: "SJ",
+    alpha3Code: "SJM",
+  },
+  {
+    alpha2Code: "SE",
+    alpha3Code: "SWE",
+  },
+  {
+    alpha2Code: "CH",
+    alpha3Code: "CHE",
+  },
+  {
+    alpha2Code: "SY",
+    alpha3Code: "SYR",
+  },
+  {
+    alpha2Code: "TW",
+    alpha3Code: "TWN",
+  },
+  {
+    alpha2Code: "TJ",
+    alpha3Code: "TJK",
+  },
+  {
+    alpha2Code: "TZ",
+    alpha3Code: "TZA",
+  },
+  {
+    alpha2Code: "TH",
+    alpha3Code: "THA",
+  },
+  {
+    alpha2Code: "TL",
+    alpha3Code: "TLS",
+  },
+  {
+    alpha2Code: "TG",
+    alpha3Code: "TGO",
+  },
+  {
+    alpha2Code: "TK",
+    alpha3Code: "TKL",
+  },
+  {
+    alpha2Code: "TO",
+    alpha3Code: "TON",
+  },
+  {
+    alpha2Code: "TT",
+    alpha3Code: "TTO",
+  },
+  {
+    alpha2Code: "TN",
+    alpha3Code: "TUN",
+  },
+  {
+    alpha2Code: "TR",
+    alpha3Code: "TUR",
+  },
+  {
+    alpha2Code: "TM",
+    alpha3Code: "TKM",
+  },
+  {
+    alpha2Code: "TC",
+    alpha3Code: "TCA",
+  },
+  {
+    alpha2Code: "TV",
+    alpha3Code: "TUV",
+  },
+  {
+    alpha2Code: "UG",
+    alpha3Code: "UGA",
+  },
+  {
+    alpha2Code: "UA",
+    alpha3Code: "UKR",
+  },
+  {
+    alpha2Code: "AE",
+    alpha3Code: "ARE",
+  },
+  {
+    alpha2Code: "GB",
+    alpha3Code: "GBR",
+  },
+  {
+    alpha2Code: "UM",
+    alpha3Code: "UMI",
+  },
+  {
+    alpha2Code: "US",
+    alpha3Code: "USA",
+  },
+  {
+    alpha2Code: "UY",
+    alpha3Code: "URY",
+  },
+  {
+    alpha2Code: "UZ",
+    alpha3Code: "UZB",
+  },
+  {
+    alpha2Code: "VU",
+    alpha3Code: "VUT",
+  },
+  {
+    alpha2Code: "VE",
+    alpha3Code: "VEN",
+  },
+  {
+    alpha2Code: "VN",
+    alpha3Code: "VNM",
+  },
+  {
+    alpha2Code: "VG",
+    alpha3Code: "VGB",
+  },
+  {
+    alpha2Code: "VI",
+    alpha3Code: "VIR",
+  },
+  {
+    alpha2Code: "WF",
+    alpha3Code: "WLF",
+  },
+  {
+    alpha2Code: "EH",
+    alpha3Code: "ESH",
+  },
+  {
+    alpha2Code: "YE",
+    alpha3Code: "YEM",
+  },
+  {
+    alpha2Code: "ZM",
+    alpha3Code: "ZMB",
+  },
+  {
+    alpha2Code: "ZW",
+    alpha3Code: "ZWE",
+  },
+  {
+    alpha2Code: "AX",
+    alpha3Code: "ALA",
+  },
+];
